d64c9d4e39c7875071f54423c9add8e848e8d42d
Paul Hanchett <pfhanchx@intel.com>
2018-06-25 15:36:40 -0700

    Modify FOR_ONELINUX.js to automatically adapt to different components.

---
+++ patch_to_oslc_context.js
@@ -0,0 +1,17 @@
+// Declares the expected input and output types of the transform:
+// Valid type names are:
+//
+// Boolean, Container, Date Time, Date, Double, Duration, Location, Long
+// Multi Select, Relationship, Relationships, Rich Text, Single Select, String
+// Web Links
+//
+var inputTypes = 'String';
+var outputTypes = 'Location';
+
+
+function transform(context, input) {
+   // returns the result of the transformation
+   var result = input.replace("Configuration-Context", "oslc.context");
+   console.log("patch_to_oslc_context: '"+input+"'-->'"+result+"'")
+   return result
+}
